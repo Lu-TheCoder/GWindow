e0f82a4afd8e15d31e72822178b7993435416b04
Lungile Maseko <90724319+Lu-TheCoder@users.noreply.github.com>
2023-10-04 23:56:58 +0200

    added comments

--- Sandbox/main.c
+++ Sandbox/main.c
@@ -3,17 +3,18 @@
 
 int main() {
 
+    //Creates a Graphics Window Instance with the specified properties
     GWindow* window = GWindow_create(800, 600, "Lu's Graphics Engine (Metal)", 0);
 
+    //GLFW inspired Loop for rendering and handling events
      while(!GWindow_should_close(window)){
         
+        //listens for OS events
         GWindow_poll_events();
-        
     }
 
+    //deallocates resources
     GWindow_destroy(window);
     
-    printf("Hellow !\n");
-
     return 0;
 }
