f9c2a6713c4a1b91213b526017e03cc3d982fd54
Lungile Maseko <90724319+Lu-TheCoder@users.noreply.github.com>
2023-10-07 12:11:40 +0200

    added Application Menu

--- GWindow/src/Platform/MacOS/ApplicationDelegate.m
+++ GWindow/src/Platform/MacOS/ApplicationDelegate.m
@@ -1,5 +1,88 @@
 #import "ApplicationDelegate.h"
 
+
+static NSString* GetApplicationName(void){
+    NSString *appName;
+    
+    /* Determine the application name */
+    appName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
+    if (!appName) {
+        appName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"];
+    }
+    
+    if (![appName length]) {
+        appName = [[NSProcessInfo processInfo] processName];
+    }
+    
+    return appName;
+}
+
+static void CreateApplicationMenus(void)
+{
+    NSMenu* mainMenu = [[NSMenu alloc] initWithTitle:@"MainMenu"];
+    
+    //App title
+    NSMenuItem* appMenuItem;
+    NSMenu* appSubmenu;
+    NSMenuItem* menuItem;
+    NSMenu* appServicesMenu = [[NSMenu alloc] initWithTitle:@"Services"];
+    
+    appMenuItem = [mainMenu addItemWithTitle:@"Apple" action:NULL keyEquivalent:@""];
+    appSubmenu = [[NSMenu alloc] initWithTitle:@"Apple"];
+    //[NSApp performSelector:NSSelectorFromString(@"setAppleMenu:") withObject:appSubmenu];
+    
+    // NSString* appName = [[NSProcessInfo processInfo] processName];
+    NSString* appName = GetApplicationName();
+    menuItem = [appSubmenu addItemWithTitle:[NSString stringWithFormat:@"%@ %@", @"About", appName] action:@selector(orderFrontStandardAboutPanel:) keyEquivalent:@""];
+    
+    [appSubmenu addItem:[NSMenuItem separatorItem]];
+    
+    menuItem = [appSubmenu addItemWithTitle:@"Preferences..." action:nil keyEquivalent:@","];
+    
+    [appSubmenu addItem:[NSMenuItem separatorItem]];
+    
+    [appSubmenu addItem:[NSMenuItem separatorItem]];
+    menuItem = [appSubmenu addItemWithTitle:@"Services" action:nil keyEquivalent:@""];
+    [menuItem setSubmenu:appServicesMenu];
+    menuItem = [appServicesMenu addItemWithTitle:@"No Services" action:nil keyEquivalent:@""];
+    [NSApp setServicesMenu:appServicesMenu];
+    
+    [appSubmenu addItem:[NSMenuItem separatorItem]];
+    
+    menuItem = [appSubmenu addItemWithTitle:[NSString stringWithFormat:@"%@ %@", @"Hide", appName] action:@selector(hide:) keyEquivalent:@"h"];
+    menuItem = [appSubmenu addItemWithTitle:@"Hide Others" action:@selector(hideOtherApplications:) keyEquivalent:@"h"];
+    [menuItem setKeyEquivalentModifierMask:NSEventModifierFlagOption | NSEventModifierFlagCommand];
+    menuItem = [appSubmenu addItemWithTitle:@"Show All" action:@selector(unhideAllApplications:) keyEquivalent:@""];
+    
+    menuItem = [appSubmenu addItemWithTitle:[NSString stringWithFormat:@"%@ %@", @"Quit", appName] action:@selector(terminate:) keyEquivalent:@"q"];
+    
+    [mainMenu setSubmenu:appSubmenu forItem:appMenuItem];
+    
+    //Window
+    NSMenuItem* windowMenuItem;
+    NSMenu* windowSubmenu;
+    
+    windowMenuItem = [mainMenu addItemWithTitle:@"Window" action:nil keyEquivalent:@""];
+    windowSubmenu = [[NSMenu alloc] initWithTitle:@"Window"];
+    
+    [windowSubmenu addItem:[NSMenuItem separatorItem]];
+    
+    menuItem = [windowSubmenu addItemWithTitle:@"Minimize" action:@selector(miniaturize:) keyEquivalent:@"m"];
+    menuItem = [windowSubmenu addItemWithTitle:@"Minimize All" action:@selector(miniaturizeAll:) keyEquivalent:@"m"];
+    [menuItem setKeyEquivalentModifierMask:NSEventModifierFlagOption | NSEventModifierFlagCommand];
+    menuItem = [windowSubmenu addItemWithTitle:@"Zoom" action:@selector(zoom:) keyEquivalent:@""];
+    
+    [windowSubmenu addItem:[NSMenuItem separatorItem]];
+    
+    menuItem = [windowSubmenu addItemWithTitle:@"Bring All to Front" action:@selector(orderFront:) keyEquivalent:@""];
+    
+    [mainMenu setSubmenu:windowSubmenu forItem:windowMenuItem];
+    
+    [NSApp setMainMenu:mainMenu];
+    [NSApp setWindowsMenu:windowSubmenu];
+}
+
+
 @implementation ApplicationDelegate
 
 -(instancetype)init
@@ -15,7 +98,7 @@
 
 -(void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-    
+    CreateApplicationMenus();   
 }
 
 - (void)applicationWillTerminate:(NSNotification *)notification
